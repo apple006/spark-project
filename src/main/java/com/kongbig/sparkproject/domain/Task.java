9757288900d4e63e866fbafc3bef377e289e6f7d
kongbig <kongbigliang@163.com>
2018-01-31 15:53:34 +0800

    1.dao模式开发 2.工厂模式的引入

--- src/main/java/com/kongbig/sparkproject/domain/Task.java
+++ src/main/java/com/kongbig/sparkproject/domain/Task.java
@@ -1,9 +1,87 @@
 package com.kongbig.sparkproject.domain;
 
+import java.io.Serializable;
+
 /**
- * Describe: 
+ * Describe: 任务
  * Author:   kongbig
  * Data:     2018/1/31.
  */
-public class Task {
+public class Task implements Serializable {
+
+    private static final long serialVersionUID = -3034224022429416380L;
+
+    private long taskId;
+    private String taskName;
+    private String createTime;
+    private String startTime;
+    private String finishTime;
+    private String taskType;
+    private String taskStatus;
+    private String taskParam;
+
+    public long getTaskId() {
+        return taskId;
+    }
+
+    public void setTaskId(long taskId) {
+        this.taskId = taskId;
+    }
+
+    public String getTaskName() {
+        return taskName;
+    }
+
+    public void setTaskName(String taskName) {
+        this.taskName = taskName;
+    }
+
+    public String getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(String createTime) {
+        this.createTime = createTime;
+    }
+
+    public String getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(String startTime) {
+        this.startTime = startTime;
+    }
+
+    public String getFinishTime() {
+        return finishTime;
+    }
+
+    public void setFinishTime(String finishTime) {
+        this.finishTime = finishTime;
+    }
+
+    public String getTaskType() {
+        return taskType;
+    }
+
+    public void setTaskType(String taskType) {
+        this.taskType = taskType;
+    }
+
+    public String getTaskStatus() {
+        return taskStatus;
+    }
+
+    public void setTaskStatus(String taskStatus) {
+        this.taskStatus = taskStatus;
+    }
+
+    public String getTaskParam() {
+        return taskParam;
+    }
+
+    public void setTaskParam(String taskParam) {
+        this.taskParam = taskParam;
+    }
+    
 }
--- src/test/java/com/kongbig/sparkproject/test/TaskDAOTest.java
+++ src/test/java/com/kongbig/sparkproject/test/TaskDAOTest.java
@@ -1,9 +1,20 @@
 package com.kongbig.sparkproject.test;
 
+import com.kongbig.sparkproject.dao.ITaskDAO;
+import com.kongbig.sparkproject.dao.impl.DAOFactory;
+import com.kongbig.sparkproject.domain.Task;
+
 /**
- * Describe:
+ * Describe: 任务管理DAO测试类
  * Author:   kongbig
  * Data:     2018/1/31.
  */
 public class TaskDAOTest {
+
+    public static void main(String[] args) {
+        ITaskDAO taskDAO = DAOFactory.getTaskDAO();
+        Task task = taskDAO.findById(1);
+        System.out.println(task.getTaskName());
+    }
+
 }
