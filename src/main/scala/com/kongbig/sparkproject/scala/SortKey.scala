e9589b78068ed33196db70bf757f896c0a4e068c
kongbigliang <kongbigliang@163.com>
2018-02-09 11:04:37 +0800

    top10热门品类之使用Scala实现二次排序并测试

--- src/main/scala/com/kongbig/sparkproject/scala/SortKey.scala
+++ src/main/scala/com/kongbig/sparkproject/scala/SortKey.scala
@@ -1,10 +1,23 @@
 package com.kongbig.sparkproject.scala
 
 /**
-  * Describe: 
+  * Describe: 品类二次排序key
   * Author:   kongbig
   * Data:     2018/2/9.
   */
-class SortKey {
+class SortKey(val clickCount: Int, val orderCount: Int, val payCount: Int)
+  extends Ordered[SortKey] with Serializable {
+
+  override def compare(that: SortKey): Int = {
+    if (clickCount - that.clickCount != 0) {
+      clickCount - that.clickCount
+    } else if (orderCount - that.orderCount != 0) {
+      orderCount - that.orderCount
+    } else if (payCount - that.payCount != 0) {
+      payCount - that.payCount
+    } else {
+      0
+    }
+  }
 
 }
--- src/main/scala/com/kongbig/sparkproject/scala/SortKeyTest.scala
+++ src/main/scala/com/kongbig/sparkproject/scala/SortKeyTest.scala
@@ -1,10 +1,28 @@
 package com.kongbig.sparkproject.scala
 
+import org.apache.spark.{SparkConf, SparkContext}
+
 /**
   * Describe: 
   * Author:   kongbig
   * Data:     2018/2/9.
   */
 object SortKeyTest {
+
+  def main(args: Array[String]): Unit = {
+    val conf = new SparkConf().setAppName("SortKeyTest").setMaster("local")
+    val sc = new SparkContext(conf)
+
+    val arr = Array(Tuple2(new SortKey(30, 35, 40), "1"),
+      Tuple2(new SortKey(35, 35, 40), "2"),
+      Tuple2(new SortKey(30, 38, 30), "3"))
+    val rdd = sc.parallelize(arr, 1)
+
+    val sortedRdd = rdd.sortByKey(false)
+
+    for (tuple <- sortedRdd.collect()) {
+      println(tuple._2)
+    }
+  }
 
 }
